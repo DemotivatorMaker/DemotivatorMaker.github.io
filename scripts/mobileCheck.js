b497187980c98b7dbbddfda56559df73ed129d52
DemotivatorMaker <89640616+DemotivatorMaker@users.noreply.github.com>
2022-02-07 17:31:59 +0500

    Add files via upload

--- scripts/mobileCheck.js
+++ scripts/mobileCheck.js
@@ -1,19 +1,23 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+function checkDevice() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
 
-    if(window.location.pathname == "/index.html"){
-        document.getElementById("defaultstyle").remove();
-        let elem = document.createElement( 'link' );
-        elem.rel = 'stylesheet';
-        elem.type = 'text/css';
-        document.body.appendChild( elem );
-        elem.href = '../styles/styleMobile.css';
-    } else if(window.location.pathname == "/defaultdemotivator.html" || window.location.pathname == "/tuneldemotivator.html") {
-        document.getElementById("defaultstyle").remove();
-        let elem = document.createElement( 'link' );
-        elem.rel = 'stylesheet';
-        elem.type = 'text/css';
-        document.body.appendChild( elem );
-        elem.href = '../styles/style2Mobile.css';
+        if(window.location.pathname == "/" || window.location.pathname == "/index.html"){
+            document.getElementById("defaultstyle").remove();
+            let elem = document.createElement( 'link' );
+            elem.rel = 'stylesheet';
+            elem.type = 'text/css';
+            document.body.appendChild( elem );
+            elem.href = '../styles/styleMobile.css';
+        } else if(window.location.pathname == "/defaultdemotivator.html" || window.location.pathname == "/tuneldemotivator.html") {
+            document.getElementById("defaultstyle").remove();
+            let elem = document.createElement( 'link' );
+            elem.rel = 'stylesheet';
+            elem.type = 'text/css';
+            document.body.appendChild( elem );
+            elem.href = '../styles/style2Mobile.css';
+        }
+    
     }
-
 }
+
+checkDevice()
